76382995346dc2c26d15559f785e95b244213194
jeongseongcheol <tjdcjf9333@gmail.com>
2024-04-19 21:24:09 +0900

    레이아웃 majagGuide 페이지 추가

--- src/components/mainImage.tsx
+++ src/components/mainImage.tsx
@@ -6,8 +6,11 @@ const mainImage :React.FC = () => {
   return (
       <div className="mainBodyCss">
         <div className="mainImageCss">
-          <Link to="/page404">
+          <Link to="/majagGuide">
             <button className="mainImageButtonCss" style = {{ border: "1px solid #457b9d", backgroundColor: "#457b9d", color:"white"}}>마작 가이드북 </button>
+          </Link>
+          <Link to="/page404">
+            <button className="mainImageButtonCss" style = {{ border: "1px solid #457b9d", backgroundColor: "#457b9d", color:"white"}}>apex 가이드북 </button>
           </Link>
 
         </div>
