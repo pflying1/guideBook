efb2a843f69bfa9cebd6dd68212d6e26e6e8e68b
jeongseongcheol <tjdcjf9333@gmail.com>
2024-04-29 18:45:34 +0900

    mysql 연결 후 테스트 예정

--- app.module.ts
+++ app.module.ts
@@ -3,31 +3,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { User } from './entities/user/user.entity';
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { User } from './entities/user/user.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(), // 환경 변수를 로드하기 위해 ConfigModule 추가
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
+      imports: [ConfigModule], // ConfigModule을 imports에 추가
+      inject: [ConfigService], // ConfigService를 TypeOrmModule 설정에 주입
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get<string>('NAS_DB_HOST'),
+        host: configService.get('NAS_DB_HOST'),
         port: configService.get<number>('NAS_DB_PORT'),
-        username: configService.get<string>('NAS_DB_USER'),
-        password: configService.get<string>('NAS_DB_PASSWORD'),
-        database: configService.get<string>('NAS_DB_NAME'),
-        entities: [User],
-        synchronize: false, // 프로덕션에서는 false로 설정
-        logging: true,
+        username: configService.get('NAS_DB_USERNAME'),
+        password: configService.get('NAS_DB_PASSWORD'),
+        database: configService.get('NAS_DB_NAME'),
+        synchronize: false,
+        entities: [
+          /* 여기에 엔티티 경로를 추가하세요 */
+        ],
       }),
-    }),
-    TypeOrmModule.forFeature([User]), // User 엔티티 등록
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'dist'),
     }),
   ],
   controllers: [AppController],
