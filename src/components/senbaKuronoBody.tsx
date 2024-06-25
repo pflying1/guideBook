d98b959373b07cac84ccdc1a94973b0f626bf01e
jeongseongcheol <tjdcjf9333@gmail.com>
2024-06-26 07:39:54 +0900

    ->키를 누르면 다음 페이지 <-키를 누르면 이전페이지 이동

--- src/components/senbaKuronoBody.tsx
+++ src/components/senbaKuronoBody.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface SenbakuronoDataInfo {
   SenbakuroKey: number;
@@ -18,6 +18,7 @@ interface SenbakuronoDataInfo {
 const SenbaKuronoBody: React.FC = () => {
   const [SenbakuronoData, setSenbakuronoData] = useState<SenbakuronoDataInfo[]>([]);
   const { chapter } = useParams<'chapter'>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchSenbakurono = async () => {
@@ -38,13 +39,42 @@ const SenbaKuronoBody: React.FC = () => {
     const chapterNumber = chapter ? chapter.replace('chapter', '') : undefined;
     // chapterNumber와 hashNumber를 사용하여 해당 항목을 찾음
     const chapterMatchCheck = SenbakuronoData.find(item => item.GuideBookAllKey === Number(chapterNumber) && item.SenbakuroContentsOrder === hashNumber);
-    
+
+    useEffect(() => {
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (chapterNumber && hashNumber) {
+          const currentChapter = Number(chapterNumber);
+          const currentHash = Number(hashNumber);
+  
+          if (event.key === "ArrowRight") {
+            // 오른쪽 키를 누르면 다음 페이지로 이동
+            const nextHash = currentHash + 1;
+            navigate(`/senbakurono/chapter${currentChapter}/#${nextHash}`);
+          } else if (event.key === "ArrowLeft") {
+            // 왼쪽 키를 누르면 이전 페이지로 이동
+            if (currentHash > 1) {
+              const prevHash = currentHash - 1;
+              navigate(`/senbakurono/chapter${currentChapter}/#${prevHash}`);
+            }
+          }
+        }
+      };
+  
+      window.addEventListener("keydown", handleKeyDown);
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [chapterNumber, hashNumber, navigate]);
+
     return (
       <div className="senbaKuronoCss">
         {chapterMatchCheck ? (
           <div className="senbakuronoWrapCss">
+            {chapterMatchCheck.SenbakuroContentsOrder === '1' ? (
             <div className="senbaKuronoTitleCss">{chapterMatchCheck.SenbakuroTitle}</div>
+            ):(
             <div className="senbaKuronoBodyCss" dangerouslySetInnerHTML={{ __html: chapterMatchCheck.SenbakuroContents }} />
+          )}
           </div>
         ) : (
           <div>해당 장의 내용을 찾을 수 없습니다.</div>
