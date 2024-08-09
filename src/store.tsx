df35b7af22a8719186ee6fb52c757187f968db04
jeongseongcheol <tjdcjf9333@gmail.com>
2024-08-09 21:39:06 +0900

    store GuideBookInfo인터페이스 추가

--- src/store.tsx
+++ src/store.tsx
@@ -1,7 +1,17 @@
 import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+// GuideBook 데이터 항목의 타입 정의
+interface GuideBookInfo {
+  GuideBookAllKey: number;
+  GuideBookAllTitle: string;
+  GuideBookAllContents: string | null;
+  updatedBy: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 // Senbakurono 데이터 항목의 타입 정의
-interface senbakuronoDataInfo {
+interface SenbakuronoDataInfo {
   SenbakuroKey: number;
   GuideBookAllKey: number;
   SenbakuroTitle: string;
@@ -14,7 +24,7 @@ interface senbakuronoDataInfo {
   updatedAt: string;
 }
 
-// GuideBook 데이터 항목의 타입 정의
+// GuideBook 데이터 항목에 Senbakurono 포함하는 타입 정의
 interface GuideBookDataInfo {
   GuideBookAllKey: number;
   GuideBookAllTitle: string;
@@ -22,7 +32,7 @@ interface GuideBookDataInfo {
   updatedBy: string;
   createdAt: string;
   updatedAt: string;
-  senbakuronoes: senbakuronoDataInfo[];
+  senbakuronoes: SenbakuronoDataInfo[];
 }
 
 // Redux 스토어의 초기 상태 타입 정의
@@ -42,19 +52,24 @@ const guideBooksSlice = createSlice({
   name: 'guideBooks',
   initialState, // 초기 상태
   reducers: {
-    // 데이터 가져올 때 상태를 설정하는 액션
+    // 단일 GuideBook 정보 업데이트를 처리하는 액션
+    setGuideBookInfo(state, action: PayloadAction<GuideBookDataInfo[]>) {
+      state.data = action.payload;
+      state.error = null;
+    },
+    // GuideBook 데이터를 설정하는 액션
     setGuideBooksData(state, action: PayloadAction<GuideBookDataInfo[]>) {
       state.data = action.payload;
       state.error = null;
     },
-     // 데이터 가져오는데 실패 할 경우 상태를 설정하는 액션
+    // 데이터 가져오는데 실패한 경우
     fetchFailure(state, action: PayloadAction<string>) {
       state.error = action.payload;
     },
   },
 });
 
-export const { setGuideBooksData, fetchFailure } = guideBooksSlice.actions;
+export const { setGuideBookInfo, setGuideBooksData, fetchFailure } = guideBooksSlice.actions;
 
 // Redux 스토어 설정
 const store = configureStore({
@@ -62,6 +77,7 @@ const store = configureStore({
     guideBooks: guideBooksSlice.reducer,
   },
 });
+
 // 루트 상태 및 디스패치 타입 정의
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
